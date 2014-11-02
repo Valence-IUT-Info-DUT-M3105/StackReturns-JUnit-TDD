1312fd00dbea0a2186a1382461341bfeb213a451
sebastienjean <baz.jean@gmail.com>
2014-11-02 18:44:14 +0100

    Iteration 2: Green
    - added a capacity field
    - added a default constructor that initializes the capacity field to
    DEFAULT_CAPACITY
    - added a constructor with parameter that initilaize the capacity field
    with a given capacity
    - updated getCapacity method so it returns the value stored in capacity
    field

--- src/fr/iutvalence/dut/info/m3105/tdd/StackOfNonNegativeIntegers.java
+++ src/fr/iutvalence/dut/info/m3105/tdd/StackOfNonNegativeIntegers.java
@@ -3,7 +3,19 @@ package fr.iutvalence.dut.info.m3105.tdd;
 public class StackOfNonNegativeIntegers
 {
 	public static final int DEFAULT_CAPACITY = 10;
+	
+	private final int capacity;
 
+	public StackOfNonNegativeIntegers()
+	{
+		this.capacity = DEFAULT_CAPACITY;
+	}
+	
+	public StackOfNonNegativeIntegers(int capacity)
+	{
+		this.capacity = capacity;
+	}
+	
 	public int getSize()
 	{
 		return 0;
@@ -11,7 +23,6 @@ public class StackOfNonNegativeIntegers
 
 	public int getCapacity()
 	{
-		return DEFAULT_CAPACITY;
+		return this.capacity;
 	}
-
 }
