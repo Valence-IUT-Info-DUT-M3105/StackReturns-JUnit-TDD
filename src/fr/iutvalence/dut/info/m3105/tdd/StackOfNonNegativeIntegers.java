115531bc1a8f583a85a2c0ec3688c1a50fc25756
sebastienjean <baz.jean@gmail.com>
2014-11-02 18:31:07 +0100

    Iteration 1: Green
    - created class StackOfNonNegativeIntegers
    - added getSize() and getCapacity() methods
    - added DEFAULT_CAPACITY constant

---
+++ src/fr/iutvalence/dut/info/m3105/tdd/StackOfNonNegativeIntegers.java
@@ -0,0 +1,17 @@
+package fr.iutvalence.dut.info.m3105.tdd;
+
+public class StackOfNonNegativeIntegers
+{
+	public static final int DEFAULT_CAPACITY = 10;
+
+	public int getSize()
+	{
+		return 0;
+	}
+
+	public int getCapacity()
+	{
+		return DEFAULT_CAPACITY;
+	}
+
+}
